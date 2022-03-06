de15281e064b92fc29c69f57109a8c9fe4c3fd3d
elisandro <elisandrogarcia1973@hotmail.com>
2022-03-06 12:43:12 -0600

    first commit

---
+++ conversor de monedas.py
@@ -0,0 +1,11 @@
+numero =int(input('escribe un numero:'))
+if numero>5:
+    print('es mayor que 5')
+elif numero==5:
+    print('es igual 5')
+else:
+     print('menor que 5')
+         
+                
+            
+         
